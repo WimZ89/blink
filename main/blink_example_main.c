972698dd66293b903edf9d86fb3513a0cee5754b
Wim Zaagman <wim.zaagman@wastevision.com>
2026-03-05 09:13:18 +0100

    soft green

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -96,7 +96,7 @@ static void blink_led(void)
     /* If the addressable LED is enabled */
     if (s_led_state) {
         /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
+        led_strip_set_pixel(led_strip, 0, 0, 10, 0);
         /* Refresh the strip to send data */
         led_strip_refresh(led_strip);
     } else {
@@ -169,7 +169,6 @@ void app_main(void)
     configure_led();
 
     while (1) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
         blink_led();
         /* Toggle the LED state */
         s_led_state = !s_led_state;
